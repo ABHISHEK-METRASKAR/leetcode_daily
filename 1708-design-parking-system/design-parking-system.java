8bf6377ccd063a837b2427027b0cb742257ff0ff
ABHISHEK-METRASKAR <abhishekmetraskar@gmail.com>
2026-04-25 23:09:10 +0530

    Time: 7 ms (100.00%) | Memory: 47.3 MB (10.76%) - LeetSync

--- 1708-design-parking-system/design-parking-system.java
+++ 1708-design-parking-system/design-parking-system.java
@@ -11,17 +11,17 @@ class ParkingSystem {
     }
     
     public boolean addCar(int carType) {
-        if (carType == 1) { // big
+        if (carType == 1) { 
             if (big > 0) {
                 big--;
                 return true;
             }
-        } else if (carType == 2) { // medium
+        } else if (carType == 2) { 
             if (medium > 0) {
                 medium--;
                 return true;
             }
-        } else if (carType == 3) { // small
+        } else if (carType == 3) { 
             if (small > 0) {
                 small--;
                 return true;

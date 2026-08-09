2d9b73cd12cbdac7cd54c765a95c1f2159a1c32b
ABHISHEK-METRASKAR <abhishekmetraskar@gmail.com>
2026-08-09 15:22:42 +0530

    Time: 3 ms (87.23%) | Memory: 65.3 MB (68.67%) - LeetSync

--- 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.java
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.java
@@ -4,19 +4,18 @@ class Solution {
         int zero = 0;
         int max = 0;
 
-        for(int right=0; right<nums.length; right++){
-            if(nums[right] == 0){
+        for (int right = 0; right < nums.length; right++) {
+            if (nums[right] == 0) {
                 zero++;
             }
-
-            while(zero > 1){
-                if(nums[left] == 0){
+            while (zero > 1) {
+                if (nums[left] == 0) {
                     zero--;
                 }
                 left++;
             }
-            max = Math.max(max,right - left);
+            max = Math.max(max, right - left);
         }
-        return max; 
+        return max;
     }
 }
